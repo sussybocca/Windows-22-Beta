2cf879361fe5d5ab8994b6894f84e9497a65322e
sussybocca <babyyodacutefry@gmail.com>
2025-10-23 07:13:14 -0500

    Update WindowsLayer.jsx

--- src/components/WindowsLayer.jsx
+++ src/components/WindowsLayer.jsx
@@ -1,17 +1,30 @@
 import React, { useContext } from "react";
 import { WindowContext } from "../contexts/WindowContext";
-import Window from "./Window";
 
 export default function WindowsLayer() {
-  const { windows } = useContext(WindowContext);
+  const { windows, closeWindow } = useContext(WindowContext);
 
   return (
-    <>
+    <div className="absolute inset-0 z-20 pointer-events-none">
       {windows.map((win) => (
-        <Window key={win.name} title={win.name} focused={win.focused}>
-          {win.component}
-        </Window>
+        <div
+          key={win.name}
+          className="absolute top-24 left-24 w-96 h-64 bg-gray-800 text-white rounded shadow-lg border border-gray-600 p-3 pointer-events-auto"
+        >
+          <div className="flex justify-between items-center bg-gray-700 p-2 rounded-t">
+            <span>{win.name}</span>
+            <button
+              onClick={() => closeWindow(win.name)}
+              className="bg-red-500 text-white px-2 rounded"
+            >
+              ✕
+            </button>
+          </div>
+          <div className="p-3">
+            <p>This is the {win.name} app.</p>
+          </div>
+        </div>
       ))}
-    </>
+    </div>
   );
 }
